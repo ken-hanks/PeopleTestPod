4c25d6c0856d185420ffb50fa16a0556b7d38587
Han Kang <hankang@people.cn>
2020-07-30 10:39:54 +0800

    Add Xcode project

---
+++ PeopleTestPod/PeopleTestPod/PeopleTestPod.h
@@ -0,0 +1,19 @@
+//
+//  PeopleTestPod.h
+//  PeopleTestPod
+//
+//  Created by KANG HAN on 2020/7/30.
+//  Copyright © 2020 KANG HAN. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PeopleTestPod.
+FOUNDATION_EXPORT double PeopleTestPodVersionNumber;
+
+//! Project version string for PeopleTestPod.
+FOUNDATION_EXPORT const unsigned char PeopleTestPodVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PeopleTestPod/PublicHeader.h>
+
+
